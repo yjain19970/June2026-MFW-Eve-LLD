52dd18e9a25b8bdaf5a7507d7ea724d94538e9e5
Yash Jain <yashjain19970@gmail.com>
2026-07-08 22:56:57 +0530

    changes

---
+++ src/main/java/org/yourcompany/scaler/mfweveningjune26/multithreading/sum1marray/SumMultiThreadCallable.java
@@ -0,0 +1,24 @@
+package org.yourcompany.scaler.mfweveningjune26.multithreading.sum1marray;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class SumMultiThreadCallable implements Callable<Long> {
+    List<Integer> arr;
+    Integer start, end;
+
+    public SumMultiThreadCallable(List<Integer> arr, Integer start, Integer end) {
+        this.arr = arr;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Long call() throws Exception {
+        Long sum = 0L;
+        for (int i = start; i < end; i++) {
+            sum += arr.get(i);
+        }
+        return sum;
+    }
+}
